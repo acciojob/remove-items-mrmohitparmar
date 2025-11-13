b776cdf50a52c732e5fdd14fc3361f458415e462
Mohit Parmar <127684291+mrmohitparmar@users.noreply.github.com>
2025-11-13 20:10:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-//your JS code here. If required.
+let btn = document.getElementById("btn");
+let select = document.getElementById("colorSelect");
+function rem() {
+	let index = select.selectedIndex;
+	if(index > (-1)){
+		select.remove(index);
+	}
+	else{
+		alert("Please select color");
+	}
+}
+btn.addEventListener("click", rem);
